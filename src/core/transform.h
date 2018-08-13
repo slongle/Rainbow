ca76eb6550c83dd13ed87b09cb0020d2a9e59670
slongle <1050106988@qq.com>
2018-08-13 22:11:40 +0800

    Add Matrix4x4 class

--- src/core/transform.h
+++ src/core/transform.h
@@ -5,7 +5,58 @@
 
 RAINBOW_NAMESPACE_BEGIN
 
+class Matrix4x4 {
+public:
+	Matrix4x4() {
+		m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1;
+		m[0][1] = m[0][2] = m[0][3] = m[1][0] = m[1][2] = m[1][3] = 
+		m[2][0] = m[2][1] = m[2][3] = m[3][0] = m[3][1] = m[3][2] = 0;
+	}
 
+	Matrix4x4(Float mat[4][4]);
+
+	bool operator == (const Matrix4x4 &m1) const {
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				if (m[i][j] != m1.m[i][j])
+					return false;
+		return true;
+	}
+
+	bool operator != (const Matrix4x4 &m1) const {
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				if (m[i][j] != m1.m[i][j])
+					return true;
+		return false;
+	}
+
+	static Matrix4x4 Mul(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+		Matrix4x4 ret;
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				ret.m[i][j] = m1.m[i][0] * m2.m[0][j] +
+				              m1.m[i][1] * m2.m[1][j] +
+				              m1.m[i][2] * m2.m[2][j] +
+                              m1.m[i][3] * m2.m[3][j];
+		return ret;
+	}
+
+	friend Matrix4x4 Transpose(const Matrix4x4 &m1);
+	friend Matrix4x4 Inverse(const Matrix4x4 &m1);
+
+	friend std::ostream &operator << (std::ostream &os, const Matrix4x4 &m1) {
+		os << "Matrix[\n" <<
+			  "  [" << m1.m[0][0] << ", " << m1.m[0][1] << ", " << m1.m[0][2] << ", " << m1.m[0][3] << "],\n" <<
+			  "  [" << m1.m[1][0] << ", " << m1.m[1][1] << ", " << m1.m[1][2] << ", " << m1.m[1][3] << "],\n" <<
+			  "  [" << m1.m[2][0] << ", " << m1.m[2][1] << ", " << m1.m[2][2] << ", " << m1.m[2][3] << "],\n" <<
+			  "  [" << m1.m[3][0] << ", " << m1.m[3][1] << ", " << m1.m[3][2] << ", " << m1.m[3][3] << "],\n" <<
+			  "]";
+		return os;
+	}
+
+	Float m[4][4];
+};
 
 RAINBOW_NAMESPACE_END
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include "core/common.h"
 #include "core/vector.h"
 #include "core/ray.h"
+#include "core/transform.h"
 using namespace rainbow;
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char *argv[]) {
-	Vector3f a(1, 2, 3);
-	Point3f b(4, 3, 2);
-	Ray c(b, a);
-	cout << c(3) << endl;
-	cout << c << endl;
+	Matrix4x4 a;
+	cout << a << endl;
 	return 0;
 }
