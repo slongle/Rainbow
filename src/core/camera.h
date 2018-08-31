0c1bb553dca98dd6977e0e6896c78ed248a8c27f
slongle <1050106988@qq.com>
2018-08-31 23:39:44 +0800

    Add camera class

--- src/core/camera.h
+++ src/core/camera.h
@@ -1,6 +1,18 @@
 #ifndef __CAMERA_H
 #define __CAMERA_H
 
+#include "common.h"
+#include "vector.h"
+#include "ray.h"
 
+RAINBOW_NAMESPACE_BEGIN
+
+class Camera {
+	Camera();
+
+	virtual Ray GenerateRay(const Point2i p) const = 0;
+};
+
+RAINBOW_NAMESPACE_END
 
 #endif // !__CAMERA_H
